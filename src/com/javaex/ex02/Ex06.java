822443be57ccc02836178aa655305112b5da8875
alsrb7342 <hi@DESKTOP-UG8GVLG>
2020-11-11 17:12:07 +0900

    2st

--- src/com/javaex/ex02/Ex06.java
+++ src/com/javaex/ex02/Ex06.java
@@ -2,7 +2,7 @@ package com.javaex.ex02;
 
 import java.util.Scanner;
 
-public class Ex05 {
+public class Ex06 {
 	
 	public static void main(String[] args) {
 		
@@ -18,14 +18,14 @@ public class Ex05 {
 		
 		int age = sc.nextInt();
 		
-		
 		System.out.println("키를 입력해주세요");
 		System.out.print("키:");
 		
-		double cm = sc.nextDouble();
+		float cm = sc.nextFloat();
+		
+		System.out.println("당신의 이름은 " + name + " 나이는 " + age + " 키는 " + cm + " 입니다");
 		
 		
-		System.out.println("당신의 이름은 " + name + " 나이는 " + age + " 키는 " + cm + " 입니다.");
 		
 		sc.close();
 	}
